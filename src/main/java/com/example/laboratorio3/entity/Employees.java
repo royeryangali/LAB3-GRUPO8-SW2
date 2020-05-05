7e510463b08eda3b1d120b8839b8e7898797be01
Sarita <atanacio.sarita@pucp.pe>
2020-05-04 20:12:33 -0500

    cambios interfaz s3

--- src/main/java/com/example/laboratorio3/entity/Employees.java
+++ src/main/java/com/example/laboratorio3/entity/Employees.java
@@ -19,6 +19,111 @@ public class Employees {
     private String email;
     private String password;
     private String phone_number;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public Date getHire_date() {
+        return hire_date;
+    }
+
+    public void setHire_date(Date hire_date) {
+        this.hire_date = hire_date;
+    }
+
+    public Jobs getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(Jobs jobs) {
+        this.jobs = jobs;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public BigDecimal getCommission_pct() {
+        return commission_pct;
+    }
+
+    public void setCommission_pct(BigDecimal commission_pct) {
+        this.commission_pct = commission_pct;
+    }
+
+    public Employees getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Employees employees) {
+        this.employees = employees;
+    }
+
+    public Departments getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(Departments departments) {
+        this.departments = departments;
+    }
+
+    public int getEnable() {
+        return enable;
+    }
+
+    public void setEnable(int enable) {
+        this.enable = enable;
+    }
+
     private Date hire_date;
     @ManyToOne
     @JoinColumn(name="job_id")
@@ -26,10 +131,10 @@ public class Employees {
     @Column(nullable = false)
     private BigDecimal salary;
     private BigDecimal commission_pct;
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name="employee_id")
     private Employees employees;
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name="department_id")
     private Departments departments;
     private int enable;
--- src/main/java/com/example/laboratorio3/entity/Jobs.java
+++ src/main/java/com/example/laboratorio3/entity/Jobs.java
@@ -1,4 +1,50 @@
 package com.example.laboratorio3.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="jobs")
 public class Jobs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="job_id")
+    private int job_history_id;
+
+    public int getJob_history_id() {
+        return job_history_id;
+    }
+
+    public void setJob_history_id(int job_history_id) {
+        this.job_history_id = job_history_id;
+    }
+
+    public String getJob_title() {
+        return job_title;
+    }
+
+    public void setJob_title(String job_title) {
+        this.job_title = job_title;
+    }
+
+    public Integer getMin_salary() {
+        return min_salary;
+    }
+
+    public void setMin_salary(Integer min_salary) {
+        this.min_salary = min_salary;
+    }
+
+    public Integer getMax_salary() {
+        return max_salary;
+    }
+
+    public void setMax_salary(Integer max_salary) {
+        this.max_salary = max_salary;
+    }
+
+    @Column(nullable = false)
+    private String job_title;
+    private Integer min_salary;
+    private Integer max_salary;
 }
