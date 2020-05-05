22ff5d8160ba6710b5fe07ceeddbe68e4aeee4c7
Royer <r.yangali@pucp.pe>
2020-05-04 20:29:27 -0500

    mejoras en employee controller royer

--- src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -4,10 +4,14 @@ import com.example.laboratorio3.entity.Employees;
 import com.example.laboratorio3.repository.EmployeesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/Employee")
@@ -38,12 +42,25 @@ public class EmployeeController {
             attr.addFlashAttribute("msg", "Empleado actualizado exitosamente");
         }
         employeesRepository.save(employee);
-        return "redirect:/product";
+        return "redirect:/Employee";
     }
 
     @GetMapping("/edit")
-    public String editarEmployee() {
+    public String editarEmployee(Model model, @RequestParam("id") int id) {
         //COMPLETAR
+        Optional<Employees> optEmployees = employeesRepository.findById(id);
+
+        if (optEmployees.isPresent()) {
+            Employees employee = optEmployees.get();
+            model.addAttribute("employee", employee);
+            model.addAttribute("listaEmpleados", employeesRepository.findAll());
+            model.addAttribute("listaJobs",supplierRepository.findAll());
+            model.addAttribute("listaDepartments",supplierRepository.findAll());
+            model.addAttribute("listaLocations",supplierRepository.findAll());
+            return "employee/editFrm";
+        } else {
+            return "redirect:/Employee";
+        }
     }
 
     @GetMapping("/delete")
