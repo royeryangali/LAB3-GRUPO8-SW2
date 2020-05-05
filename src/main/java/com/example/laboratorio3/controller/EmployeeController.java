4c503c58085461d6af63f9a43f865af38597bd8c
Royer <r.yangali@pucp.pe>
2020-05-04 20:21:01 -0500

    commit royer con america/zone y ssl

--- src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -1,6 +1,10 @@
 package com.example.laboratorio3.controller;
 
-//COMPLETAR
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Employee")
 public class EmployeeController {
 
     //COMPLETAR
