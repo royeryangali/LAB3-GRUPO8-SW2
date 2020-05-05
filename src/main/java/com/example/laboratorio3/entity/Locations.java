31801f646cb525a6b8ca7047935158e0986f2abb
Royer <r.yangali@pucp.pe>
2020-05-04 20:02:33 -0500

    segundo commit royer con locations entity getter y setter

--- src/main/java/com/example/laboratorio3/entity/Locations.java
+++ src/main/java/com/example/laboratorio3/entity/Locations.java
@@ -25,5 +25,51 @@ public class Locations {
     @JoinColumn(name = "country_id")
     private Countries country;
 
+    public int getLocationid() {
+        return locationid;
+    }
 
+    public void setLocationid(int locationid) {
+        this.locationid = locationid;
+    }
+
+    public String getStreetaddress() {
+        return streetaddress;
+    }
+
+    public void setStreetaddress(String streetaddress) {
+        this.streetaddress = streetaddress;
+    }
+
+    public String getPostalcode() {
+        return postalcode;
+    }
+
+    public void setPostalcode(String postalcode) {
+        this.postalcode = postalcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStateprovince() {
+        return stateprovince;
+    }
+
+    public void setStateprovince(String stateprovince) {
+        this.stateprovince = stateprovince;
+    }
+
+    public Countries getCountry() {
+        return country;
+    }
+
+    public void setCountry(Countries country) {
+        this.country = country;
+    }
 }
