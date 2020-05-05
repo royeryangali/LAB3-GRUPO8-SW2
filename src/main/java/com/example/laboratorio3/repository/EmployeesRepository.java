36bc91c5f5b8b90d0d8ed7549bb16278378f16e8
Royer <r.yangali@pucp.pe>
2020-05-04 20:59:48 -0500

    dto royer

--- src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
+++ src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
@@ -18,4 +18,7 @@ public interface EmployeesRepository extends JpaRepository<Employees, Integer> {
             "WHERE e.salary >15000",nativeQuery = true)
     List <EmployeeDto> obtenerempleadosSueldoMayor();
 
+    @Query("SELECT e.first_name, e.last_name, e.hire_date,j.job_title FROM employees e INNER JOIN jobs j on (e.job_id=j.job_id) INNER JOIN departments d on (e.department_id=d.department_id)
+
+    ORDER BY e.salary GROUP BY d.department_id", nativeQuery=tru)
 }
