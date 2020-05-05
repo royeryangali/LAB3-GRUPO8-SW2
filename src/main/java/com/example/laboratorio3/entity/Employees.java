5909c479bea8938d5947adfb65ebbc2723ae1e7a
Leonardo Ayala <driss_leo_1@hotmail.com>
2020-05-04 19:59:55 -0500

    employee - LEO

--- src/main/java/com/example/laboratorio3/entity/Employees.java
+++ src/main/java/com/example/laboratorio3/entity/Employees.java
@@ -1,12 +1,39 @@
 package com.example.laboratorio3.entity;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.util.Date;
 
-//COMPLETAR
+
+@Entity
+@Table(name="employees")
 public class Employees {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="employee_id")
+    private int id;
+    private String first_name;
+    @Column(nullable = false)
+    private String last_name;
+    @Column(nullable = false)
+    private String email;
+    private String password;
+    private String phone_number;
+    private Date hire_date;
+    @ManyToOne
+    @JoinColumn(name="job_id")
+    private Jobs jobs;
+    @Column(nullable = false)
+    private BigDecimal salary;
+    private BigDecimal commission_pct;
+    @OneToMany
+    @JoinColumn(name="employee_id")
+    private Employees employees;
+    @OneToMany
+    @JoinColumn(name="department_id")
+    private Departments departments;
+    private int enable;
+
 
            //COMPLETAR
 }
